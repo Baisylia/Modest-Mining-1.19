f9a5ee5a9166da8cd123f973d5e5cfa943f8b1b9
evanbones <74276616+evanbones@users.noreply.github.com>
2026-08-05 01:30:31 -0700

    javelin reworks

--- src/main/java/com/baisylia/modestmining/entity/custom/ThrownJavelinEntity.java
+++ src/main/java/com/baisylia/modestmining/entity/custom/ThrownJavelinEntity.java
@@ -5,18 +5,34 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
+
+import javax.annotation.Nullable;
 
 public class ThrownJavelinEntity extends AbstractArrow {
 
     private static final EntityDataAccessor<ItemStack> DATA_JAVELIN_STACK =
             SynchedEntityData.defineId(ThrownJavelinEntity.class, EntityDataSerializers.ITEM_STACK);
-
+    private static final EntityDataAccessor<Byte> DATA_LOYALTY =
+            SynchedEntityData.defineId(ThrownJavelinEntity.class, EntityDataSerializers.BYTE);
+    private static final EntityDataAccessor<Boolean> DATA_FOIL =
+            SynchedEntityData.defineId(ThrownJavelinEntity.class, EntityDataSerializers.BOOLEAN);
+    public int clientSideReturnTridentTickCount;
     private ItemStack javelinStack = ItemStack.EMPTY;
+    private boolean dealtDamage;
 
     public ThrownJavelinEntity(EntityType<? extends AbstractArrow> type, Level level) {
         super(type, level);
@@ -26,6 +42,8 @@ public class ThrownJavelinEntity extends AbstractArrow {
         super(ModEntityTypes.THROWN_JAVELIN.get(), shooter, level);
         this.javelinStack = stack.copy();
         this.entityData.set(DATA_JAVELIN_STACK, stack.copy());
+        this.entityData.set(DATA_LOYALTY, (byte) EnchantmentHelper.getLoyalty(stack));
+        this.entityData.set(DATA_FOIL, stack.hasFoil());
     }
 
     @Override
@@ -33,16 +51,129 @@ public class ThrownJavelinEntity extends AbstractArrow {
         return this.entityData.get(DATA_JAVELIN_STACK);
     }
 
+    public boolean isFoil() {
+        return this.entityData.get(DATA_FOIL);
+    }
+
     @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(DATA_JAVELIN_STACK, ItemStack.EMPTY);
+        this.entityData.define(DATA_LOYALTY, (byte) 0);
+        this.entityData.define(DATA_FOIL, false);
+    }
+
+    @Override
+    public void tick() {
+        if (this.inGroundTime > 4) {
+            this.dealtDamage = true;
+        }
+
+        Entity owner = this.getOwner();
+        int loyaltyLevel = this.entityData.get(DATA_LOYALTY);
+        if (loyaltyLevel > 0 && (this.dealtDamage || this.isNoPhysics()) && owner != null) {
+            if (!this.isAcceptableReturnOwner()) {
+                if (!this.level.isClientSide && this.pickup == AbstractArrow.Pickup.ALLOWED) {
+                    this.spawnAtLocation(this.getPickupItem(), 0.1F);
+                }
+                this.discard();
+            } else {
+                this.setNoPhysics(true);
+                Vec3 vec3 = owner.getEyePosition().subtract(this.position());
+                this.setPosRaw(this.getX(), this.getY() + vec3.y * 0.015D * (double) loyaltyLevel, this.getZ());
+                if (this.level.isClientSide) {
+                    this.yOld = this.getY();
+                }
+
+                double d0 = 0.05D * (double) loyaltyLevel;
+                this.setDeltaMovement(this.getDeltaMovement().scale(0.95D).add(vec3.normalize().scale(d0)));
+                if (this.clientSideReturnTridentTickCount == 0) {
+                    this.playSound(SoundEvents.TRIDENT_RETURN, 10.0F, 1.0F);
+                }
+
+                ++this.clientSideReturnTridentTickCount;
+            }
+        }
+
+        super.tick();
+    }
+
+    private boolean isAcceptableReturnOwner() {
+        Entity entity = this.getOwner();
+        if (entity != null && entity.isAlive()) {
+            return !(entity instanceof ServerPlayer) || !entity.isSpectator();
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    @Nullable
+    protected EntityHitResult findHitEntity(Vec3 pStartVec, Vec3 pEndVec) {
+        return this.dealtDamage ? null : super.findHitEntity(pStartVec, pEndVec);
+    }
+
+    @Override
+    protected void onHitEntity(EntityHitResult pResult) {
+        Entity entity = pResult.getEntity();
+        float f = (float) this.getBaseDamage();
+        if (entity instanceof LivingEntity livingentity) {
+            f += EnchantmentHelper.getDamageBonus(this.getPickupItem(), livingentity.getMobType());
+        }
+
+        Entity owner = this.getOwner();
+        DamageSource damagesource = DamageSource.trident(this, owner == null ? this : owner);
+        this.dealtDamage = true;
+        SoundEvent soundevent = SoundEvents.TRIDENT_HIT;
+        if (entity.hurt(damagesource, f)) {
+            if (entity.getType() == EntityType.ENDERMAN) {
+                return;
+            }
+
+            if (entity instanceof LivingEntity livingentity1) {
+                if (owner instanceof LivingEntity livingOwner) {
+                    EnchantmentHelper.doPostHurtEffects(livingentity1, livingOwner);
+                    EnchantmentHelper.doPostDamageEffects(livingOwner, livingentity1);
+                }
+
+                this.doPostHurtEffects(livingentity1);
+            }
+        }
+
+        this.setDeltaMovement(this.getDeltaMovement().multiply(-0.01D, -0.1D, -0.01D));
+        this.playSound(soundevent, 1.0F, 1.0F);
+    }
+
+    @Override
+    protected boolean tryPickup(Player pPlayer) {
+        return super.tryPickup(pPlayer) || (this.isNoPhysics() && this.ownedBy(pPlayer) && pPlayer.getInventory().add(this.getPickupItem()));
+    }
+
+    @Override
+    protected SoundEvent getDefaultHitGroundSoundEvent() {
+        return SoundEvents.TRIDENT_HIT_GROUND;
+    }
+
+    @Override
+    public void playerTouch(Player player) {
+        if (this.ownedBy(player) || this.getOwner() == null) {
+            super.playerTouch(player);
+        }
+    }
+
+    @Override
+    public void tickDespawn() {
+        int loyaltyLevel = this.entityData.get(DATA_LOYALTY);
+        if (this.pickup != AbstractArrow.Pickup.ALLOWED || loyaltyLevel <= 0) {
+            super.tickDespawn();
+        }
     }
 
     @Override
     public void addAdditionalSaveData(CompoundTag tag) {
         super.addAdditionalSaveData(tag);
         tag.put("Javelin", this.javelinStack.save(new CompoundTag()));
+        tag.putBoolean("DealtDamage", this.dealtDamage);
     }
 
     @Override
@@ -51,6 +182,9 @@ public class ThrownJavelinEntity extends AbstractArrow {
         if (tag.contains("Javelin")) {
             this.javelinStack = ItemStack.of(tag.getCompound("Javelin"));
             this.entityData.set(DATA_JAVELIN_STACK, this.javelinStack.copy());
+            this.entityData.set(DATA_LOYALTY, (byte) EnchantmentHelper.getLoyalty(this.javelinStack));
+            this.entityData.set(DATA_FOIL, this.javelinStack.hasFoil());
         }
+        this.dealtDamage = tag.getBoolean("DealtDamage");
     }
 }
--- src/main/java/com/baisylia/modestmining/item/custom/weapons/JavelinItem.java
+++ src/main/java/com/baisylia/modestmining/item/custom/weapons/JavelinItem.java
@@ -3,38 +3,40 @@ package com.baisylia.modestmining.item.custom.weapons;
 import com.baisylia.modestmining.entity.custom.ThrownJavelinEntity;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
-import net.minecraft.sounds.SoundEvent;
+import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.TridentItem;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.ForgeMod;
 
 import java.util.UUID;
 
-public class JavelinItem extends TridentItem {
+public class JavelinItem extends Item implements Vanishable {
 
     private static final UUID ATTACK_RANGE_MODIFIER = UUID.fromString("63d316c1-7d6d-41be-81c3-41fc1a216c27");
 
+    private final Tier tier;
     private final Multimap<Attribute, AttributeModifier> defaultModifiers;
     private final float throwDamage;
 
     public JavelinItem(Tier tier, float attackDamage, float attackSpeed, float reach, Properties properties) {
         super(properties);
+        this.tier = tier;
         this.throwDamage = attackDamage + tier.getAttackDamageBonus();
 
         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
@@ -43,6 +45,55 @@ public class JavelinItem extends TridentItem {
         builder.put(ForgeMod.ATTACK_RANGE.get(), new AttributeModifier(ATTACK_RANGE_MODIFIER, "Weapon modifier", reach, AttributeModifier.Operation.ADDITION));
 
         this.defaultModifiers = builder.build();
+    }
+
+    @Override
+    public boolean canAttackBlock(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer) {
+        return !pPlayer.isCreative();
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack pStack) {
+        return UseAnim.SPEAR;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack pStack) {
+        return 72000;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        if (itemstack.getDamageValue() >= itemstack.getMaxDamage() - 1) {
+            return InteractionResultHolder.fail(itemstack);
+        } else {
+            pPlayer.startUsingItem(pHand);
+            return InteractionResultHolder.consume(itemstack);
+        }
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        pStack.hurtAndBreak(1, pAttacker, (entity) -> {
+            entity.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+        });
+        return true;
+    }
+
+    @Override
+    public boolean mineBlock(ItemStack pStack, Level pLevel, BlockState pState, BlockPos pPos, LivingEntity pEntityLiving) {
+        if ((double) pState.getDestroySpeed(pLevel, pPos) != 0.0D) {
+            pStack.hurtAndBreak(2, pEntityLiving, (entity) -> {
+                entity.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+            });
+        }
+        return true;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.tier.getEnchantmentValue();
     }
 
     @Override
@@ -55,60 +106,35 @@ public class JavelinItem extends TridentItem {
         if (pEntityLiving instanceof Player player) {
             int i = this.getUseDuration(pStack) - pTimeLeft;
             if (i >= 10) {
-                int j = EnchantmentHelper.getRiptide(pStack);
-                if (j <= 0 || player.isInWaterOrRain()) {
-                    if (!pLevel.isClientSide) {
-                        pStack.hurtAndBreak(1, player, (p_43388_) -> {
-                            p_43388_.broadcastBreakEvent(pEntityLiving.getUsedItemHand());
-                        });
-                        if (j == 0) {
-                            ThrownJavelinEntity javelin = new ThrownJavelinEntity(pLevel, player, pStack);
-                            javelin.setBaseDamage(this.throwDamage);
-                            javelin.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 2.5F + (float) j * 0.5F, 1.0F);
-                            if (player.getAbilities().instabuild) {
-                                javelin.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
-                            }
-                            pLevel.addFreshEntity(javelin);
-                            pLevel.playSound(null, javelin, SoundEvents.TRIDENT_THROW, SoundSource.PLAYERS, 1.0F, 1.0F
-                            );
-                            if (!player.getAbilities().instabuild) {
-                                player.getInventory().removeItem(pStack);
-                            }
-                        }
+                if (!pLevel.isClientSide) {
+                    pStack.hurtAndBreak(1, player, (p_43388_) -> {
+                        p_43388_.broadcastBreakEvent(pEntityLiving.getUsedItemHand());
+                    });
+                    ThrownJavelinEntity javelin = new ThrownJavelinEntity(pLevel, player, pStack);
+                    javelin.setBaseDamage(this.throwDamage);
+                    javelin.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 2.5F, 1.0F);
+                    if (player.getAbilities().instabuild) {
+                        javelin.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
                     }
-
-                    player.awardStat(Stats.ITEM_USED.get(this));
-                    if (j > 0) {
-                        float f7 = player.getYRot();
-                        float f = player.getXRot();
-                        float f1 = -Mth.sin(f7 * ((float) Math.PI / 180F)) * Mth.cos(f * ((float) Math.PI / 180F));
-                        float f2 = -Mth.sin(f * ((float) Math.PI / 180F));
-                        float f3 = Mth.cos(f7 * ((float) Math.PI / 180F)) * Mth.cos(f * ((float) Math.PI / 180F));
-                        float f4 = Mth.sqrt(f1 * f1 + f2 * f2 + f3 * f3);
-                        float f5 = 3.0F * ((1.0F + (float) j) / 4.0F);
-                        f1 *= f5 / f4;
-                        f2 *= f5 / f4;
-                        f3 *= f5 / f4;
-                        player.push(f1, f2, f3);
-                        player.startAutoSpinAttack(20);
-                        if (player.isOnGround()) {
-                            player.move(MoverType.SELF, new Vec3(0.0D, 1.1999999F, 0.0D));
-                        }
-
-                        SoundEvent soundevent;
-                        if (j >= 3) {
-                            soundevent = SoundEvents.TRIDENT_RIPTIDE_3;
-                        } else if (j == 2) {
-                            soundevent = SoundEvents.TRIDENT_RIPTIDE_2;
-                        } else {
-                            soundevent = SoundEvents.TRIDENT_RIPTIDE_1;
-                        }
-
-                        pLevel.playSound(null, player, soundevent, SoundSource.PLAYERS, 1.0F, 1.0F);
+                    pLevel.addFreshEntity(javelin);
+                    pLevel.playSound(null, javelin, SoundEvents.TRIDENT_THROW, SoundSource.PLAYERS, 1.0F, 1.0F);
+                    if (!player.getAbilities().instabuild) {
+                        player.getInventory().removeItem(pStack);
                     }
-
                 }
+                player.awardStat(Stats.ITEM_USED.get(this));
             }
         }
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        if (enchantment == Enchantments.LOYALTY) {
+            return true;
+        }
+        if (enchantment.category == EnchantmentCategory.TRIDENT) {
+            return false;
+        }
+        return super.canApplyAtEnchantingTable(stack, enchantment);
     }
 }
